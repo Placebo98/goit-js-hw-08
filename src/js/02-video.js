e4638e8dcc2931de6dd95bffa259b1be37d963bf
Placebo98 <oleg.tsaryk98@gmail.com>
2023-06-25 19:49:21 +0300

    added changes for hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoCurrentTime = 'videoplayer-current-time';
+const playerEl = document.querySelector('#vimeo-player');
+const player = new Player(playerEl);
+const currentTime = localStorage.getItem(videoCurrentTime);
+
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem(videoCurrentTime, JSON.stringify(currentTime));
+  });
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+function remuveVideo() {
+  player.setCurrentTime(currentTime || 0);
+}
+remuveVideo();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const throttle = require('lodash.throttle');
+
+const feedbackStorage = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const formEmail = formEl.querySelector('input');
+const formMessage = formEl.querySelector('textarea');
+
+let output = JSON.parse(localStorage.getItem(feedbackStorage)) || {
+  email: '',
+  message: '',
+};
+console.log(output);
+
+formEmail.value = output.email;
+formMessage.value = output.message;
+
+formEl.addEventListener('input', throttle(setFeedbackForm, 500));
+
+function setFeedbackForm(event) {
+  event.preventDefault();
+  output[event.target.name] = event.target.value;
+  localStorage.setItem(feedbackStorage, JSON.stringify(output));
+}
+formEl.addEventListener('submit', throttle(getFeedbackForm, 500));
+
+function getFeedbackForm(event) {
+  event.preventDefault();
+  console.log(output);
+  event.target.reset();
+  output = { email: '', message: '' };
+  localStorage.removeItem(feedbackStorage);
+}
